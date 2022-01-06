7c0609559f4df0c91e16a483b278459afab96139
Samwell90 <samedwardsphone@gmail.com>
2022-01-06 20:40:26 +0000

    first commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+        domains: ['links.papareact.com'],
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1Ijoic2Ftd2VsbDkwIiwiYSI6ImNrdmo5dWptNTAza2QycW91MnBnOWxjamMifQ.ZIEGFD2n1l257v9Z4H2PXQ',
+    }
+}
